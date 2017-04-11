0d6555bc9be14cfb86cb14415556100375bad1c1
tsokura <toni.sokura@monoko.to>
2017-04-11 16:09:08 +0100

    updated

--- colorpicker-arduino/colorpicker-arduino.ino
+++ colorpicker-arduino/colorpicker-arduino.ino
@@ -12,70 +12,60 @@ String pixelMessage = "";
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRBW + NEO_KHZ800);
 
-void setup() {
+int delayval = 50; // delay for half a second
+int ColVal[] = {50,0,0,0};
+
+int cnt;
+
+void setup()
+{
+  Serial.begin(9600);
   pixels.begin();
-  Serial.begin(9600); // Start serial communication at 9600 bps
 }
 
-
-void serialEvent() {
-  while (Serial.available()) {
-    char inChar = (char)Serial.read();
-    pixelMessage += inChar;
-    if (inChar == '\n') {
-      messageComplete = true;
-      String parsedMessage = parseMessage(pixelMessage);
-      renderPixels(parsedMessage);
-    }
+void loop()
+{
+  if (Serial.available())
+  {
+   char chan = Serial.read(); //channel
+   int val = Serial.parseInt();//value
+   setval(chan, val);
+   String data = (String(ColVal[0]) + " "+ String(ColVal[1]) +" " + String(ColVal[2]));
+   Serial.println(data); 
   }
 }
 
-void loop() {
-  if (messageComplete) {
-    Serial.println(pixelMessage);
-    pixelMessage = "";
-    messageComplete = false;
+void setval(char col, int dataV)
+{ 
+  if (col == 'r')
+  {
+   ColVal[0] = dataV;
+   cnt ++;
+  }
+  if (col == 'g')
+  {
+    ColVal[1] = dataV;
+    cnt ++;
+  }
+  if (col == 'b')
+  {
+   ColVal[2] = dataV;
+   cnt ++;
+  }
+  ;
+  if (cnt % 3 == 0)
+  {
+    go();
   }
 }
 
-String parseMessage(String _pixelMessage) {
-  //turn a pixelMessage into a set of instructions
-  //does nothing at the moment
-  return _pixelMessage;
-}
-
-void renderPixels(String _pixelMessage) {
-  //output the pixels to the display
-  for (int i = 0; i < _pixelMessage.length(); i++) {
-    char msgChar = _pixelMessage.charAt(i);
-    //ugh
-    if (i < NUMPIXELS) {
-      switch (msgChar) {
-        case 'r':
-          pixels.setPixelColor(i, pixels.Color(255, 0, 0));
-          break;
-        case 'g':
-          pixels.setPixelColor(i, pixels.Color(0, 255, 0));
-          break;
-        case 'b':
-          pixels.setPixelColor(i, pixels.Color(0, 0, 255));
-          break;
-        case 'w':
-          pixels.setPixelColor(i, pixels.Color(255, 255, 255));
-          break;
-        case '8':
-          pixels.setPixelColor(i, pixels.Color(200, 200, 200));
-          break;
-        case '6':
-          pixels.setPixelColor(i, pixels.Color(100, 100, 100));
-          break;
-        default:
-          pixels.setPixelColor(i, pixels.Color(0, 0, 0));
-          break;
-      }
-    }
+void go()
+{
+  ;
+  for(int i=0;i<NUMPIXELS;i++) //Climb LED with received colour value
+  {
+    pixels.setPixelColor(i, pixels.Color(ColVal[0],ColVal[1],ColVal[2]));
+    pixels.show(); // This sends the updated pixel color to the hardware
+    delay(delayval); // Delay for a period of time (in milliseconds).
   }
-  pixels.show();
-  Serial.println("render");
 }
-
